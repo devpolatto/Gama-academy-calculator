eb3d110e9ab09a42179117ab8cdbe23af87babad
Angelo Polatto <polatto.xcode.job@gmail.com>
2022-08-20 18:24:25 -0300

    feat: function that clears one number at a time

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const display = document.getElementById('operation-result');
 //seleciona todas as teclas de numeros
 const numbers = document.querySelectorAll('[id*=key-number]');
 const btnClear = document.getElementById('key-c')
+const btnClearOne = document.getElementById('key-ce')
 
 //atualiza o display com o valor capturado do click
 const updateDisplay = (text) => {
@@ -15,3 +16,9 @@ numbers.forEach (number => number.addEventListener('click', insertNumber));
 
 // limpa o display
 btnClear.addEventListener('click', () => display.innerHTML = "")
+
+// elimina um numero
+btnClearOne.addEventListener('click', () => {
+     let valueResult = display.innerHTML
+     display.innerHTML = valueResult.substring(0, valueResult.length -1)
+})
