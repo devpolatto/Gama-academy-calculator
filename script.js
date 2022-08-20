c675b2b3ee0e410c1b32d45db1473e8ce953ed54
Angelo Polatto <polatto.xcode.job@gmail.com>
2022-08-20 18:15:22 -0300

    feat: function to clear the display

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const display = document.getElementById('operation-result');
 //seleciona todas as teclas de numeros
 const numbers = document.querySelectorAll('[id*=key-number]');
+const btnClear = document.getElementById('key-c')
 
 //atualiza o display com o valor capturado do click
 const updateDisplay = (text) => {
-    display.textContent += text;
+     display.textContent += text;
 }
 //captura o valor do click
 const insertNumber = (event) => updateDisplay(event.target.textContent);
 
 //adiciona um evento de click para toda tecla de numero
 numbers.forEach (number => number.addEventListener('click', insertNumber));
+
+// limpa o display
+btnClear.addEventListener('click', () => display.innerHTML = "")
