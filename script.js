5b3add5f43ba6738e640460bd3196568306f04e7
Angelo Polatto <polatto.xcode.job@gmail.com>
2022-08-21 16:47:06 -0300

    feat: adding calculate function

--- script.js
+++ script.js
@@ -3,16 +3,19 @@ const display = document.getElementById('operation-result');
 const numbers = document.querySelectorAll('[id*=key-number]');
 const btnClear = document.getElementById('key-c')
 const btnClearOne = document.getElementById('key-ce')
+const btnOperator = document.querySelectorAll('[id*=key-operator]')
+const btnEqual = document.getElementById('key-equal')
 
 //atualiza o display com o valor capturado do click
 const updateDisplay = (text) => {
      display.textContent += text;
 }
 //captura o valor do click
-const insertNumber = (event) => updateDisplay(event.target.textContent);
+const getClick = (event) => updateDisplay(event.target.textContent);
 
-//adiciona um evento de click para toda tecla de numero
-numbers.forEach (number => number.addEventListener('click', insertNumber));
+//adiciona um evento de click para toda tecla de numero e operador
+numbers.forEach (number => number.addEventListener('click', getClick));
+btnOperator.forEach(operator => operator.addEventListener('click', getClick))
 
 // limpa o display
 btnClear.addEventListener('click', () => display.innerHTML = "")
@@ -22,3 +25,11 @@ btnClearOne.addEventListener('click', () => {
      let valueResult = display.innerHTML
      display.innerHTML = valueResult.substring(0, valueResult.length -1)
 })
+
+function cacl() {
+     if(display.innerHTML) {
+          display.innerHTML = eval(display.innerHTML)
+     }
+}
+
+btnEqual.addEventListener('click', cacl)
